2ce62609e4377709f242149e2a4814e7b3a56ae1
Foucz <tomekandr@gmail.com>
2019-01-11 16:35:27 +0100

    Dodano podstawową forme rejestracji i jej css

---
+++ register.php
@@ -0,0 +1,22 @@
+ <form action="action_page.php">
+  <div class="container">
+  <link rel="stylesheet" type="text/css" href="styles/register.css">
+    <h1>Rejestracja</h1>
+
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Wpisz Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Wpisz Haslo" name="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Powtorz Haslo Password" name="psw-repeat" required>
+    <hr>
+
+    <button type="submit" class="registerbtn">Rejestruj</button>
+  </div>
+
+  </div>
+</form> 
