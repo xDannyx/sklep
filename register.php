f706cc634934d96f7c1c83fe6e63ec34c4702e61
Foucz <tomekandr@gmail.com>
2019-01-17 19:10:00 +0100

    ponowna zmiana pliku

--- register.php
+++ register.php
@@ -5,8 +5,8 @@
 	      $email = $_POST['email'];
         $password = $_POST['password'];
  
-        $query = "INSERT INTO `klienci` (login, hash_hasla, imie) VALUES ('$username', '$password', '$email)";
-        $result = mysqli_query($connection, $query);
+        $query = "INSERT INTO `user` (username, password, email) VALUES ('$username', '$password', '$email')";
+        $result = mysqli_query($conn, $query);
         if($result){
             $smsg = "User Created Successfully.";
         }else{
@@ -14,7 +14,8 @@
         }
     }
  ?>
-<head>
+
+ <head>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
@@ -26,19 +27,19 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-</head>
-
-	
+ </head>
 <div class="container">
       <form class="form-signin" method="POST">
         <h2 class="form-signin-heading">Please Register</h2>
         <div class="input-group">
+	  <span class="input-group-addon" id="basic-addon1">@</span>
 	  <input type="text" name="username" class="form-control" placeholder="Username" required>
 	</div>
-        <label for="inputEmail" class="sr-only">email</label>
+        <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
         <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
       </form>
+</div>
